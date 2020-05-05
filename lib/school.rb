7a847628df1311ea00f27b7fdbd670ce505d659c
Joseph Romero <joeromero47@gmail.com>
2020-05-05 20:40:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,10 @@ class School
     @roster = {}
   end
   def add_student(name, grade)
-    if @roster
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] =
     @roster[grade] = []
     @roster[grade] << name
   end
